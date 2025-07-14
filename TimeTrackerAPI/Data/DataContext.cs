c1b9321b1b484df5ac3392b50159500403d20539
Andrew Sabin <117333125+andrew-sabin@users.noreply.github.com>
2025-07-13 21:41:41 -0700

    Change the DataContext and Models

--- TimeTrackerAPI/Data/DataContext.cs
+++ TimeTrackerAPI/Data/DataContext.cs
@@ -1,14 +1,21 @@
-﻿namespace TimeTrackerAPI.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TimeTrackerAPI.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional model configurations can be added here
+        }
         public DbSet<TimeEntry> TimeEntries { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectDetails> ProjectDetails { get; set; }
-        public DbSet<User> Users { get; set; }
     }
 }
